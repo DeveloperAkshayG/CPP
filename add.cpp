e04fbf675d6d5204d74f9966457876b7d8a8aa48
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2021-07-10 21:15:09 +0530

    Create add.cpp
    demonstration of cin to take user input,cout to ask user to enter value and endl to insert new line

---
+++ add.cpp
@@ -0,0 +1,14 @@
+//demonstration of cout,cin and endl in addition program
+#include <iostream>  
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout << "enter value of a"<<endl;
+    cin >> a;
+    cout << "enter value of b"<<endl;
+    cin >> b; 
+    c=a+b;
+    cout << "the result is:" <<c<<endl;
+    return 0;
+}
