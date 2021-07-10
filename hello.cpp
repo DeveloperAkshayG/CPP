f82c5cfdb62e6b5877b6d66377e7fcf8f85d1553
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2021-07-10 20:58:58 +0530

    Create hello.cpp
    without "using namespace std"

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+//this is the basic cpp code without using namespace std
+//to avoid "using namespace std"in main function i have used std::cout<<"hello world"
+#include <iostream>  
+int main()
+{
+    std::cout << "hello world";
+    return 0;
+}
