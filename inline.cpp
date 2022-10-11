bf4204f3f5679ecfbec8fc5a512a6a3adab28105
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2022-10-11 15:56:52 +0530

    inline function with integer as parameter

---
+++ inline.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+//inline funntion below
+inline void printme(int x)
+{
+    cout << x << endl;
+}
+
+int main()
+{
+//    cout<<"Hello World";
+    for(int i=0;i<=10;i++)
+    {
+        printme(i);
+    }
+    
+
+    return 0;
+}
