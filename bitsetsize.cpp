bc4ca4b453c1401bb49271c6c0d43ee1d1b9e16e
unknown <agajare22@gmail.com>
2022-11-18 17:35:12 +0530

    bitset size() in cpp

---
+++ bitsetsize.cpp
@@ -0,0 +1,13 @@
+
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+
+#include<iostream>
+#include<bitset>
+using namespace std;
+
+int main()
+{
+    bitset<8>b("10101010");
+    cout<<"size of b: " << b.size() <<endl; //to determine the size of bitset or total number of bits
+    return 0;
+}
